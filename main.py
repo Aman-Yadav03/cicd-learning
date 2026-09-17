e9876d9eb885b7da35ad2cd97d08e917ff3773b8
Aman Yadav <amanyadav0542vns@gmail.com>
2026-09-17 12:56:54 +0530

    Create initial FastAPI learning app

---
+++ main.py
@@ -0,0 +1,25 @@
+
+from fastapi import FastAPI
+
+app = FastAPI(title="CI/CD Learning API")
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello from my CI/CD learning project!"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.get("/version")
+def version():
+    return {
+        "version": "1.0.0"
+    }
